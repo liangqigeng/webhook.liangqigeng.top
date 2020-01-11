f365b214b69a1806ef4392634f0fb087353ef1b2
liangqigeng <422592842@qq.com>
2020-01-12 04:16:06 +0800

    111

--- webhook.php
+++ webhook.php
@@ -12,9 +12,9 @@ $path = "/www/wwwroot/webhook.liangqigeng.top";
 $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
 
 if($signature){
-   $hash = "shal=".hash_hmac('sha1',file_get_contents("php://input"),$secret);
+   $hash = "sha1=".hash_hmac('sha1',file_get_contents("php://input"),$secret);
    if(strcmp($signature,$hash)==0){
-    echo shell_exec("cd {$path} && /usr/bin/git reset --hard orign/master && /usr/bin/git reset --hard orign/master && /user/bin/git clean -f && /user/bin/git pull 2>&1");
+    echo shell_exec("cd {$path} && /usr/bin/git reset --hard orign/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
   exit();
 }
 }
